c860bbf28e0ea78abfbe615bf73c4f89bb2f674b
Syderi <syderi@yahoo.com>
2022-07-13 15:14:14 +0300

    fix: end test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 
 // You should implement your task here.
 
+let result = [];
+
 module.exports = function towelSort (matrix) {
-  return [];
+  console.log(10, matrix);
+  if (!matrix) {
+    return result;
+  };
+
+  matrix.forEach((element, index) => {
+    if (index === 0) {
+      result = [...element];
+      console.log(15, result);
+
+    }
+    else if (index % 2) {
+      result = [...result, ...(element.reverse())];
+      console.log(20, result);
+    } else {
+      result = [...result, ...element];
+      console.log(25, result);
+    }
+  });
+
+  console.log(30, result);
+  return result;
 }
+
